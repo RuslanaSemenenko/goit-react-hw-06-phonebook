6bca031e4f2273a73447f33a40844e1c66fbb607
Ruslana Semenenko <katotochkaaa@gmail.com>
2023-06-24 19:00:20 +0100

    homework

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,23 +1,12 @@
-import { useDispatch } from 'react-redux';
-import { deleteContact } from 'redux/contactsSlice';
 import './Contact.module.jsx';
 import PropTypes from 'prop-types';
 
 export const Contact = ({ contact }) => {
-  const dispatch = useDispatch();
-
-  const handleDelete = () => {
-    dispatch(deleteContact(contact.id));
-  };
-
   return (
     <div className="List_item">
       <span>
         {contact.name}: <span className="List_span">{contact.number}</span>
       </span>
-      {/* <button className="List_button" onClick={handleDelete}>
-        delete
-      </button> */}
     </div>
   );
 };
