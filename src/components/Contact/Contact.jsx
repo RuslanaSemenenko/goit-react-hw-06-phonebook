46013ee9b6e26dd457e499d64243d378bdc1774e
Ruslana Semenenko <katotochkaaa@gmail.com>
2023-06-24 22:30:46 +0100

    homework

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,16 +1,37 @@
-import './Contact.module.jsx';
-import PropTypes from 'prop-types';
+// import './Contact.module.jsx';
+// import PropTypes from 'prop-types';
+
+// export const Contact = ({ contact }) => {
+//   return (
+//     <div className="List_item">
+//       <span>
+//         {contact.name}: <span className="List_span">{contact.number}</span>
+//       </span>
+//     </div>
+//   );
+// };
+
+// Contact.propTypes = {
+//   contact: PropTypes.object,
+// };
+
+import React from 'react';
+import { useDispatch } from 'react-redux';
+import { deleteContact } from 'redux/contactsSlice';
 
 export const Contact = ({ contact }) => {
+  const dispatch = useDispatch();
+
+  const handleDelete = () => dispatch(deleteContact(contact.id));
+
   return (
     <div className="List_item">
       <span>
         {contact.name}: <span className="List_span">{contact.number}</span>
       </span>
+      <button className="List_button" onClick={handleDelete}>
+        delete
+      </button>
     </div>
   );
-};
-
-Contact.propTypes = {
-  contact: PropTypes.object,
 };
--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,15 +1,57 @@
+// import React from 'react';
+// import PropTypes from 'prop-types';
+// import { useSelector } from 'react-redux';
+// import { getContacts, getFilter } from 'redux/selectors';
+// import { Contact } from 'components/Contact/Contact';
+// import {
+//   TaskList,
+//   TaskListItem,
+//   TaskListButton,
+// } from './ContactList.module.jsx';
+
+// const ContactList = ({ onRemoveContact }) => {
+//   const contacts = useSelector(getContacts);
+//   const filter = useSelector(getFilter);
+
+//   if (!contacts || !Array.isArray(contacts)) {
+//     return null;
+//   }
+
+//   const visibleContacts = contacts.filter(contact =>
+//     contact.name.toLowerCase().includes(filter.toLowerCase())
+//   );
+
+//   return (
+//     <TaskList className="List_box">
+//       {visibleContacts.map(contact => (
+//         <TaskListItem className="List_item" key={contact.id}>
+//           <Contact contact={contact} />
+//           <TaskListButton
+//             type="button"
+//             name="delete"
+//             onClick={() => onRemoveContact(contact.id)}
+//           >
+//             delete
+//           </TaskListButton>
+//         </TaskListItem>
+//       ))}
+//     </TaskList>
+//   );
+// };
+
+// export default ContactList;
+
+// ContactList.propTypes = {
+//   onRemoveContact: PropTypes.func.isRequired,
+// };
+
 import React from 'react';
-import PropTypes from 'prop-types';
 import { useSelector } from 'react-redux';
 import { getContacts, getFilter } from 'redux/selectors';
 import { Contact } from 'components/Contact/Contact';
-import {
-  TaskList,
-  TaskListItem,
-  TaskListButton,
-} from './ContactList.module.jsx';
+import { TaskList, TaskListItem } from './ContactList.module.jsx';
 
-const ContactList = ({ onRemoveContact }) => {
+const ContactList = () => {
   const contacts = useSelector(getContacts);
   const filter = useSelector(getFilter);
 
@@ -26,13 +68,6 @@ const ContactList = ({ onRemoveContact }) => {
       {visibleContacts.map(contact => (
         <TaskListItem className="List_item" key={contact.id}>
           <Contact contact={contact} />
-          <TaskListButton
-            type="button"
-            name="delete"
-            onClick={() => onRemoveContact(contact.id)}
-          >
-            delete
-          </TaskListButton>
         </TaskListItem>
       ))}
     </TaskList>
@@ -40,7 +75,3 @@ const ContactList = ({ onRemoveContact }) => {
 };
 
 export default ContactList;
-
-ContactList.propTypes = {
-  onRemoveContact: PropTypes.func.isRequired,
-};
